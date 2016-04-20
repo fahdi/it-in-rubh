d6816505c0d6688274c09134249b242842bce824
Fahad Murtaza <fahdim@gmail.com>
2016-04-20 09:41:20 +0400

    Division in floating point numbers and some explanation in comments

--- 004-weird-arithmetic.rb
+++ 004-weird-arithmetic.rb
@@ -2,3 +2,16 @@ puts(9 / 2)
 
 # Output:
 #   4
+# ABove code outputs 4 as integrer as both numbers are integers
+
+
+
+# If both or just one of the franction numbers are in floating point,
+# then the  output would be a floating point number
+
+puts(9.0 / 2)
+puts(9 / 2.0)
+
+# Output:
+#   4.5
+#   4.5
