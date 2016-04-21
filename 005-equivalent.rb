894709ed2f8cf119ed6f9bbe7e819131a0e36b6c
Fahad Murtaza <fahdim@gmail.com>
2016-04-21 09:53:57 +0400

    Integer division and modulo operations, more examples

--- 005-equivalent.rb
+++ 005-equivalent.rb
@@ -2,3 +2,9 @@
 11 / 4 == 2
 # The remainder of dividing 11 by 4 is 3:
 11 % 4 == 3
+
+# 4 divides 12 three times:
+12 / 4 == 3
+# the remainder is zero:
+12 % 4 == 0
+# therefore 4 divides 12 evenly
