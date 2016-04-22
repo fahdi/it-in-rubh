843839bc608f811f39881111e65ccc6c7012b0dc
Fahad Murtaza <fahdim@gmail.com>
2016-04-22 11:55:31 +0400

    Intro to variables

---
+++ 006-variables.rb
@@ -0,0 +1,8 @@
+puts("Racecars, Lasers, Aeroplanes")
+puts("Racecars, Lasers, Aeroplanes")
+puts("Racecars, Lasers, Aeroplanes")
+
+# Output:
+#   Racecars, Lasers, Aeroplanes
+#   Racecars, Lasers, Aeroplanes
+#   Racecars, Lasers, Aeroplanes
