76e9e8499f954b229dc9e84d8b40fe98828d3e11
Fahad Murtaza <fahdim@gmail.com>
2016-04-22 11:57:46 +0400

    Changing multiple print statements into a single variable

--- 006-variables.rb
+++ 006-variables.rb
@@ -6,3 +6,15 @@ puts("Racecars, Lasers, Aeroplanes")
 #   Racecars, Lasers, Aeroplanes
 #   Racecars, Lasers, Aeroplanes
 #   Racecars, Lasers, Aeroplanes
+
+# Making above into variables 
+
+cool_things = "Racecars, Lasers, Aeroplanes"
+puts(cool_things)
+puts(cool_things)
+puts(cool_things)
+
+# Output:
+#   Racecars, Lasers, Aeroplanes
+#   Racecars, Lasers, Aeroplanes
+#   Racecars, Lasers, Aeroplanes
