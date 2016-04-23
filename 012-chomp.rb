d08f7b507949c0c070a6dc91d2e7faeac50ca7c5
Fahad Murtaza <fahdim@gmail.com>
2016-04-23 13:21:01 +0400

    Adding input chomping example with input being chomped in second input

--- 012-chomp.rb
+++ 012-chomp.rb
@@ -6,3 +6,11 @@ puts("Hello " + name + "!")
 
 puts("No input chomping so the new line for '!'")
 
+# With inout chomping 
+
+puts("Type in your name!")
+name = gets.chomp()
+puts("Hello " + name + "!")
+
+puts("Notice all in one line printing above. Chomp Chomp!")
+
