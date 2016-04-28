7063e902c5b1f129e5c144704535c86ce4777d12
Fahad Murtaza <fahdim@gmail.com>
2016-04-29 02:32:14 +0400

    if else conditions

---
+++ 018-if-else-condition.rb
@@ -0,0 +1,10 @@
+puts("Type in a number")
+num = gets.to_i
+
+if num < 10
+  puts("That's not a big number!")
+else
+  puts("Wow, that's a big number!")
+end
+
+puts("Thanks for typing in a number!")
