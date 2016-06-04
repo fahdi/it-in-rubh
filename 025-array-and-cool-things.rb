1e04dbc0dae10e52e66ffa2ae3efe87fcb7c59a0
Fahad Murtaza <fahdim@gmail.com>
2016-06-04 12:41:18 +0400

    Array and cool things

---
+++ 025-array-and-cool-things.rb
@@ -0,0 +1,13 @@
+cool_things = ["Racecars", "Lasers", "Aeroplanes"]
+four_primes = [2, 3, 5, 7]
+an_empty_array = []
+
+puts(cool_things[0])
+puts(cool_things[1])
+puts(cool_things[2])
+
+puts(four_primes[0])
+puts(four_primes[1])
+puts(four_primes[2])
+
+puts(an_empty_array[0])
