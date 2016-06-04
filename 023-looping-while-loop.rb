9598855f75071fa6c4ccd9886438d57cb674714a
Fahad Murtaza <fahdim@gmail.com>
2016-06-04 11:38:43 +0400

    Add while loop, more changes

--- 023-looping-while-loop.rb
+++ 023-looping-while-loop.rb
@@ -3,5 +3,7 @@ num = gets.to_i
 
 while num > 100
 	puts("The number is too high, please inout a smaller number")
-	num = get.to_i
+	num = gets.to_i
 end	
+
+puts("You typed " + num.to_s + " which is less than or equal to 100. Good work.")
