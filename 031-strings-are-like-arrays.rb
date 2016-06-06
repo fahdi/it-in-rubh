257c06cdf0139576fb72d09ddf65d7294a4606e2
Fahad Murtaza <fahdim@gmail.com>
2016-06-06 18:21:25 +0400

    String array join

--- 031-strings-are-like-arrays.rb
+++ 031-strings-are-like-arrays.rb
@@ -1,4 +1,7 @@
-#Note that even though strings are similar to arrays of letters, they are not really the same:
+# Note that even though strings are similar to arrays of letters, they are not really the same:
 
 puts("abc" == ["a", "b", "c"]) # prints false 
 
+# Explaining a little more
+
+puts("abc" == ["a", "b", "c"].join)
