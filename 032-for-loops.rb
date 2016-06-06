89a85dbb7fb96d053fa505406a4a6c17361a9107
Fahad Murtaza <fahdim@gmail.com>
2016-06-06 18:18:45 +0400

    For loops

---
+++ 032-for-loops.rb
@@ -0,0 +1,12 @@
+#fizzbuzz exercise, iterating from 1 to 100
+for x in 1..100    
+    if x % 15 == 0
+        puts "fizzbuzz"
+    elsif x % 3 == 0
+        puts "fizz"
+    elsif x % 5 == 0
+        puts "buzz"
+    else
+        puts "fizzbuzz"
+    end
+end
